aa7c236c96fe4e49c452b1e0e58bff42f29863a6
Clever Machaca <106214364+cleverjohann@users.noreply.github.com>
2025-02-14 11:44:11 -0500

    Inicializacion del proyecto con Spring boot y configuracion de PostgreSQL

---
+++ src/main/java/com/example/discord_scheduler/DiscordSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.example.discord_scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscordSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscordSchedulerApplication.class, args);
+	}
+
+}
